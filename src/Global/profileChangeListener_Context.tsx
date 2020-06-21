b99ad0d204151a53e6a73f15a24559f94cad26cb
v-gutierrez <victorgutierrezgomes@gmail.com>
2020-06-20 21:11:47 -0400

    fix

--- src/Global/profileChangeListener_Context.tsx
+++ src/Global/profileChangeListener_Context.tsx
@@ -2,9 +2,6 @@ import React, { createContext, useState } from 'react';
 
 export const profileChangeListener = createContext<any>(null);
 
-// Segundo as minhas pesquisas realmente existe um problema para tipar
-// createContext no TypeScript. Manterei como any até encontrar uma solução
-
 export function Provider({ children }) {
     const [contextState, setContextState] = useState<number>();
 
